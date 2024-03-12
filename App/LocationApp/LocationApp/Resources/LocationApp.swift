4e787596a411661e9c06451f58d0fb3bc2f0d35d
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-03-13 01:29:26 +0900

    Init : Create Location App

---
+++ App/LocationApp/LocationApp/Resources/LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationAppApp.swift
+//  LocationApp
+//
+//  Created by 승재 on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
