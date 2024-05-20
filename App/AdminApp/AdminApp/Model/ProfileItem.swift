f9d27572f214bcfa9ec27df66f988e01051f39ab
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-05-20 14:52:07 +0900

    Update: 프로필 아이템 수정
    * title 이름 변경

--- App/AdminApp/AdminApp/Model/ProfileItem.swift
+++ App/AdminApp/AdminApp/Model/ProfileItem.swift
@@ -23,7 +23,7 @@ struct ProfileItem : Hashable{
     ]
     static let otherlist: [ProfileItem] = [
         ProfileItem(title: "기기 로그아웃", content: .logout),
-        ProfileItem(title: "회원 탈퇴", content: .deleteUser),
-        ProfileItem(title: "개인정보 처리 방침", content: .logout)
+        ProfileItem(title: "계정 탈퇴신청", content: .deleteUser),
+//        ProfileItem(title: "개인정보 처리 방침", content: .logout)
     ]
 }
